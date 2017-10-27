91e64334519dea28d8fa873f98edad4e6a11b89d
zhouhaiming <joamingzhm@163.com>
2017-10-28 00:32:49 +0800

    主页

---
+++ app/src/main/java/cn/eud/pku/joaming/selectdormitory/MainActivity.java
@@ -0,0 +1,19 @@
+package cn.eud.pku.joaming.selectdormitory;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by joaming on 2017/10/27.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainlayout);
+    }
+
+
+}
